29d0397ac0019984121a836d54a6a78d873a9837
Tomislav-lovric <tomislav.lovric.321@gmail.com>
2023-04-11 17:10:20 +0200

    init

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,108 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Book")
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "book_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "book_name",
+            nullable = false
+    )
+    private String bookName;
+
+    @Column(
+            name = "created_at",
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE",
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @ManyToOne()
+    @JoinColumn(
+            name = "student_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "student_book_fk"
+            )
+    )
+    private Student student;
+
+    public Book(String bookName, LocalDateTime createdAt, Student student) {
+        this.bookName = bookName;
+        this.createdAt = createdAt;
+        this.student = student;
+    }
+
+    public Book(String bookName, LocalDateTime createdAt) {
+        this.bookName = bookName;
+        this.createdAt = createdAt;
+    }
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookName='" + bookName + '\'' +
+                ", createdAt=" + createdAt +
+                ", student=" + student +
+                '}';
+    }
+}
